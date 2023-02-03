1f5f63f700cb579fb1c03d45809e35c981496691
Tyler Jennings <tylerjennings2@outlook.com>
2023-02-03 16:29:56 -0600

    FEATURE-TAKE-PHOTO: Implemented photo capturing using CameraX

--- app/src/main/java/com/example/a3plwinnervisitorcheckinapp/MainActivity.java
+++ app/src/main/java/com/example/a3plwinnervisitorcheckinapp/MainActivity.java
@@ -1,21 +1,44 @@
 package com.example.a3plwinnervisitorcheckinapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.content.ContentValues.TAG;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 import java.util.Properties;
-
-import javax.mail.Authenticator;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
@@ -26,14 +49,29 @@ import javax.mail.internet.MimeMessage;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String FILENAME_FORMAT = "dd-M-yyyy hh:mm:ss";
+    public static final int REQUEST_CODE_PERMISSIONS = 10;
+    public static final String TAG = "CameraXApp";
+    private ExecutorService cameraExecutor;
+    private static final String[] REQUIRED_PERMISSIONS;
+
+    static {
+        List<String> requiredPermissions = new ArrayList<>(Arrays.asList(android.Manifest.permission.CAMERA));
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            requiredPermissions.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        REQUIRED_PERMISSIONS = requiredPermissions.toArray(new String[0]);
+    }
+
     private String mFirstName, mLastName;
     private EditText edtFirstName, edtLastName;
     private Button btnCheckIn;
+
+    private ImageCapture imageCapture = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // prevents users from rotating screen
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
@@ -42,7 +80,12 @@ public class MainActivity extends AppCompatActivity {
         edtFirstName = findViewById(R.id.edtMainScreenFirstName);
         edtLastName = findViewById(R.id.edtMainScreenLastName);
         btnCheckIn = findViewById(R.id.btnMainScreenSubmit);
-
+        imageCapture = new ImageCapture.Builder().build();
+        if (allPermissionsGranted()) {
+            startCamera();
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
         btnCheckIn.setOnClickListener(v -> {
             mFirstName = edtFirstName.getText().toString();
             mLastName = edtLastName.getText().toString();
@@ -55,11 +98,16 @@ public class MainActivity extends AppCompatActivity {
             }
             if(!mFirstName.isEmpty() && !mLastName.isEmpty()) {
                 // take photo and send information through email
-                sendEmail();
+                takePhoto();
+                //sendEmail();
+
             }
         });
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+
+        cameraExecutor = Executors.newSingleThreadExecutor();
+
     }
 
     private void sendEmail() {
@@ -97,4 +145,92 @@ public class MainActivity extends AppCompatActivity {
         LocalDateTime now = LocalDateTime.now();
         return dtf.format(now);
     }
+
+    private void takePhoto() {
+        ImageCapture imageCapture = this.imageCapture;
+        if (imageCapture == null) return;
+
+        SimpleDateFormat nameFormat = new SimpleDateFormat(FILENAME_FORMAT, Locale.US);
+        String name = nameFormat.format(System.currentTimeMillis());
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image");
+        }
+
+        ImageCapture.OutputFileOptions outputOptions = new ImageCapture.OutputFileOptions.Builder(
+                getContentResolver(),
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+        ).build();
+
+        imageCapture.takePicture(
+                outputOptions,
+                Executors.newSingleThreadExecutor(),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Log.e(TAG, "Photo capture failed: " + exception.getMessage(), exception);
+                    }
+
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        String msg = "Photo capture succeeded: " + outputFileResults.getSavedUri();
+                        Log.d(TAG, msg);
+                    }
+                });
+    }
+    private void startCamera() {
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                //Preview preview = new Preview.Builder().build();
+                //preview.setSurfaceProvider(viewBinding.viewFinder.getSurfaceProvider());
+
+                CameraSelector cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA;
+
+                try {
+                    cameraProvider.unbindAll();
+                    cameraProvider.bindToLifecycle(MainActivity.this, cameraSelector, imageCapture);
+                } catch (Exception exception) {
+                    Log.e(TAG, "Use case binding failed", exception);
+                }
+            } catch (ExecutionException | InterruptedException e) {
+                // Exception handling
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraExecutor.shutdown();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera();
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
 }
