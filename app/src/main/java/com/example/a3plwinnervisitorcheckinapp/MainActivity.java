bd61c22a56923c06b54068c86dab37f7665d810a
Tyler Jennings <tylerjennings2@outlook.com>
2023-02-03 20:19:13 -0600

    BUGFIX-TAKE-PHOTO: Found proper path to image

--- app/src/main/java/com/example/a3plwinnervisitorcheckinapp/MainActivity.java
+++ app/src/main/java/com/example/a3plwinnervisitorcheckinapp/MainActivity.java
@@ -12,14 +12,21 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ContentValues;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.StrictMode;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
@@ -29,6 +36,7 @@ import android.widget.Toast;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -118,13 +126,17 @@ public class MainActivity extends AppCompatActivity {
             if(!mFirstName.isEmpty() && !mLastName.isEmpty() &&
                 !mWhoAreYouVisiting.isEmpty() && !mReason.isEmpty()) {
                 // take photo and send information through email
-                sendEmail();
-                Intent i = new Intent(MainActivity.this, ConfirmationScreen.class);
-                startActivity(i);
-                edtFirstName.getText().clear();
-                edtLastName.getText().clear();
-                edtWhoAreYouVisiting.getText().clear();
-                edtReason.getText().clear();
+                takePhoto();
+                final Handler handler = new Handler(Looper.getMainLooper());
+                handler.postDelayed(() -> {
+                    sendEmail();
+                    Intent i = new Intent(MainActivity.this, ConfirmationScreen.class);
+                    startActivity(i);
+                    edtFirstName.getText().clear();
+                    edtLastName.getText().clear();
+                    edtWhoAreYouVisiting.getText().clear();
+                    edtReason.getText().clear();
+                }, 3000);
             }
         });
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -135,7 +147,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendEmail() {
-        takePhoto();
         final String username="3plwinnerwms@gmail.com";
         final String password="rfemloyjgbiqnsak";
         final String recipient = "jorgem@3plwinner.com";
@@ -161,16 +172,15 @@ public class MainActivity extends AppCompatActivity {
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
 
 
-
             message.setSubject("3PLWINNER Visitor Check-In - "+ getCurrentTime());
 
             MimeMultipart multipart = new MimeMultipart();
 
             MimeBodyPart attachment = new MimeBodyPart();
-            DataSource source = new FileDataSource(path);
+            String filePath = getPath(this, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            DataSource source = new FileDataSource(filePath);
 
-            attachment.attachFile(path);
-            attachment.setFileName(fileName);
+            attachment.setFileName(fileName+".jpg");
             attachment.setDataHandler(new DataHandler(source));
 
             MimeBodyPart messageBodyPart = new MimeBodyPart();
@@ -184,8 +194,6 @@ public class MainActivity extends AppCompatActivity {
             Transport.send(message);
 
         }catch (MessagingException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
@@ -230,8 +238,6 @@ public class MainActivity extends AppCompatActivity {
                         Log.d(TAG, msg);
                     }
                 });
-        path = outputOptions.getFile().getAbsolutePath();
-
     }
     private void startCamera() {
         ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
@@ -281,6 +287,126 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return true;
+    }
+
+    public static String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context The context.
+     * @param uri The Uri to query.
+     * @param selection (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
 
 
